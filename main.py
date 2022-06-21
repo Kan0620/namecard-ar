163e7b917323de7c2a38eb4aad16d67623408459
kan <kantasoworld0620@gmail.com>
2022-06-21 23:22:01 +0900

    add: descrition

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from routers import index, test
+from settings.settings import api_info, tags_info
 
-app = FastAPI()
+app = FastAPI(
+    title=api_info["title"],
+    description=api_info["description"],
+    version=api_info["version"],
+    openapi_tags=tags_info
+)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(index.router)
-app.include_router(test.router)
+app.include_router(index.router, tags=["index"])
+app.include_router(test.router,tags=["marker-AR"])
 
 # if __name__ == '__main__':
 #     # コンソールで [$ uvicorn run:app --reload]でも可
---
+++ settings/settings.py
@@ -0,0 +1,22 @@
+api_description = \
+"""
+### ARを表示するHTMLを返すAPI テスト開発用
+"""
+
+api_info = {
+    "title": "AR API",
+    "description": api_description,
+    "version": "0.1.0",
+    "prefix": ""
+}
+
+tags_info = [
+    {
+        "name": "index",
+        "description": "appが動いてるか確認する"
+    },
+    {
+        "name": "marker-AR",
+        "description": "マーカーを用いたAR"
+    }
+]
