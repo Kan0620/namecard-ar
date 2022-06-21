443259bfdc4c634d75c367180d38bc51c51e988b
kan <kantasoworld0620@gmail.com>
2022-06-21 20:10:44 +0900

    modified: state path

--- routers/test_ar.py
+++ routers/test_ar.py
@@ -6,8 +6,8 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
-router.mount("../static", StaticFiles(directory="../static"), name="static")
-templates = Jinja2Templates(directory='../static')
+router.mount("static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='static')
 
 @router.get("/test_ar", response_class=HTMLResponse)
 async def test_ar(request: Request):
