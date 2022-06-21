feb51b4be26885cadc625cfdabc421578f8f64ab
kan <kantasoworld0620@gmail.com>
2022-06-21 20:07:31 +0900

    modified: embed path

--- main.py
+++ main.py
@@ -14,18 +14,27 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from routers import index, test_ar
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory='static')
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/test", response_class=HTMLResponse)
 
 def get_product(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request,
+            "obj_file_path": "data/first_test.glb",
+            "marker_img_path": "data/pattern-marker.patt"
+         }
     )
+
+app.include_router(index.router)
+app.include_router(test_ar.router)
 
 # if __name__ == '__main__':
 #     # コンソールで [$ uvicorn run:app --reload]でも可
---
+++ routers/test_ar.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+
+router.mount("../static", StaticFiles(directory="../static"), name="static")
+templates = Jinja2Templates(directory='../static')
+
+@router.get("/test_ar", response_class=HTMLResponse)
+async def test_ar(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
