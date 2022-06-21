1bde5544c94d8adef2da829a154324b18d9de712
kan <kantasoworld0620@gmail.com>
2022-06-21 19:42:57 +0900

    modified: use url_for

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ import uvicorn
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory='pages')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='static')
 
 
 @app.get("/", response_class=HTMLResponse)
